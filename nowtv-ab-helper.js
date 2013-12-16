2c14bf0f7081333e90675ce9fc613abea00c2f04
popcorn <blah@blah.org>
2013-12-16 15:49:04 +0000

    @chris @lukasz nowtv ab helper

---
+++ nowtv-ab-helper.js
@@ -0,0 +1,15 @@
+var segmentation = {
+    isXboxUser:function () {
+        console.log('is xbox user');
+    }
+};
+
+var experience = {
+    setNoCrossSellPage:function () {
+        console.log('no cross sell page');
+    }
+}
+
+
+window.NOWTV_SEGMENTATION = segmentation;
+window.NOWTV_EXPERIENCE = experience;
